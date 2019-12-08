0b074378406641523dd3be9047e76207ecf6f7a4
Heather Moore <moore.hj@gmail.com>
2019-12-08 22:01:12 +0100

    user secrets

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
+import os
 
 app = Flask(__name__)
 app.run(debug=True, host='0.0.0.0')
@@ -19,9 +20,9 @@ login.login_view = 'login'
 from app import routes, models
 
 if not models.User.query.filter_by(username='admin').first():
-    user = models.User(username='admin')
-    user.set_two_factor('12345678901')
-    user.set_password('Administrator@1')
+    user = models.User(username=os.environ.get('ADMIN_USER'))
+    user.set_two_factor(os.environ.get('ADMIN_MFA'))
+    user.set_password(os.environ.get('ADMIN_PASSWORD'))
     user.set_admin(True)
     db.session.add(user)
     db.session.commit()
